94ab9753f997a0560a4fb7ce833f380f7f3c14e8
markfullmer <mfullmer@gmail.com>
2014-09-19 15:18:01 -0500

    initial commit of small image subtheme, based on production as of 9/18/2014

--- functions.php
+++ functions.php
@@ -1,71 +1,25 @@
 <?php
-// Custom Header -- make ours slightly different than the Responsive parent theme:
-// Note: the parent theme has code that users custom-header and get_custom_header .. which is WP 3.4+
-// We'll need to update our override code here when we upgrade our server.
-define('HEADER_IMAGE_WIDTH', 960); 
-define('HEADER_IMAGE_HEIGHT', 320);
-define('HEADER_IMAGE', '%s/../responsive-child-theme/images/default-logo.png'); // %s is the template dir uri
 
-function UTbranding() 
-{
-    echo'<div id="UTbranding">
-    <a href="http://www.utexas.edu">
-        <img src="http://www.utexas.edu/opa/graphics/2dtower.jpg" alt="The University of Texas at Austin"   width="320" height="44">
-    </a>
-    </div>
-    ';
-}
+// Exit if accessed directly
+if ( !defined('ABSPATH')) exit;
 
-add_filter('responsive_in_header','UTbranding');
-
-function UTfooter_links()
-{
-    echo '<div class="grid col-940 clearfix fit centered">
-                  <a href="http://www.utexas.edu/its/">Information Technology Services</a>
-                  <br/>
-                        <a href="http://www.utexas.edu/policies/privacy/">Web Privacy Policy</a> &nbsp;|&nbsp; 
-                        <a href="http://www.utexas.edu/brand-guidelines/web-guidelines/accessibility">Web Accessibility</a>
- <br/>
- <br/>
-  </div>';
-}
-
-add_filter('responsive_footer','UTfooter_links');
-
-// Using WP's i18n to stamp our name and link into the footer
-// Idea came from here: http://blog.ftwr.co.uk/archives/2010/01/02/mangling-strings-for-fun-and-profit/
-class UT_Text_Wrangler {
-    function powered_by_text($translation, $text, $domain) {
-        $translations = &get_translations_for_domain( $domain );
-        if ( $text == 'Responsive Theme') {
-            return $translations->translate( 'UT Austin Responsive Theme');
-        } elseif ($text == 'http://themeid.com/responsive-theme/') {
-            return $translations->translate('http://blogs.utexas.edu/utresponsive/');
-        }
-        return $translation;
-    }
-}
-
-add_filter('gettext', array('UT_Text_Wrangler', 'powered_by_text'), 10, 4);
-
-// the functions responsive_template_data() and responsive_theme_data() add
-// some <meta> tags with unregistered metadata keywords ("template" and
-// "theme"), which fails HTML validation. So, let's remove them. Can't just
-// remove_action(), because the parent theme calls add_action() after this
-// file runs.  So we insert this function as an action in get_header
-// (before wp_head) to remove those actions from wp_head.
-function remove_meta_tags() {
-    remove_action('wp_head', 'responsive_template_data');
-    remove_action('wp_head', 'responsive_theme_data');
-}
-
-add_action("get_header","remove_meta_tags");
-
-// Added an extra hook inside the #footer div
-function responsive_footer() 
-{
-    do_action('responsive_footer');
-}
-
-
+/**
+ * Search Form Template
+ *
+ *
+ * @file           searchform.php
+ * @package        Responsive 
+ * @author         Emil Uzelac 
+ * @copyright      2003 - 2012 ThemeID
+ * @license        license.txt
+ * @version        Release: 1.0
+ * @filesource     wp-content/themes/responsive/searchform.php
+ * @link           http://codex.wordpress.org/Function_Reference/get_search_form
+ * @since          available since Release 1.0
+ */
 ?>
+    <form method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+        <label for="s">Search</label>
+        <input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e('search here &hellip;', 'responsive'); ?>" />
+        <input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e('Go', 'responsive'); ?>"  />
+    </form>
